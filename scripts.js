0381e1ccff6ab593dd6a5bc2332a16e5c1d5a505
Sabrina Inczedy Pagan <sinczedy@gmail.com>
2026-03-08 23:12:29 -0400

    Add script for video and email message handling

---
+++ scripts.js
@@ -0,0 +1,14 @@
+<script>
+        function videoFunction(){
+            document.getElementById("myComment").innerHTML += "A great vacation video"
+        }
+</script>
+
+
+function emailMessage() { 
+    document.getElementById("emailBox").innerHTML += " Please enter a valid email address"; 
+}
+
+function blankMessage() {
+    document.getElementById("emailBox").innerHTML = "Email:";
+}
